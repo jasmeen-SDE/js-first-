1736d763b0ab812aeacfe78d338c707df330721e
jasmeen-SDE <sandhujasmeen623@gmail.com>
2026-06-27 22:04:25 +0530

    First Java script code

---
+++ code.js
@@ -0,0 +1,82 @@
+
+
+/* Print Output */
+console.log("Hello, World!");
+console.log("Welcome to JavaScript!");
+
+/* Variables */
+let name = "Jasmeen";
+let age = 20;
+
+console.log(name);
+console.log(age);
+
+/* Arithmetic Operators */
+let a = 20;
+let b = 10;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+/* Increment and Decrement */
+let number = 5;
+
+number++;
+console.log(number);
+
+number--;
+console.log(number);
+
+
+
+/* Logical Operators */
+let Student = true;
+
+console.log(age >= 18 && Student);
+console.log(age >= 18 || Student);
+console.log(!Student);
+
+/* If Statement */
+if (age >= 18) {
+    console.log("You can vote.");
+}
+
+/* If Else Statement */
+let marks = 80;
+
+if (marks >= 33) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
+/* Else If Statement */
+if (marks >= 90) {
+    console.log("Grade A");
+} else if (marks >= 75) {
+    console.log("Grade B");
+} else if (marks >= 50) {
+    console.log("Grade C");
+} else {
+    console.log("Fail");
+}
+
+
+
+
+
+
+/* Object */
+let student = {
+    name: "Jasmeen",
+    age: 20,
+    course: "JavaScript"
+};
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.course);
+
